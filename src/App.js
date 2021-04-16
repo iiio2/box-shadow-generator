1961fc691af5e577ab6069e4d7025203fce0548f
iiio2 <gomexxxx71@gmail.com>
2021-04-16 22:04:31 +0600

    first commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [hori, setHori] = useState(10);
+  const [veri, setVeri] = useState(10);
+  const [blur, setBlur] = useState(10);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='controls'>
+        <label htmlFor='horizontal'>Horizontal Height</label>
+        <input
+          type='range'
+          min='5'
+          max='100'
+          value={hori}
+          onChange={(e) => setHori(e.target.value)}
+        />
+        <label htmlFor='Vertical'>Vertical Height</label>
+        <input
+          type='range'
+          min='5'
+          max='100'
+          value={veri}
+          onChange={(e) => setVeri(e.target.value)}
+        />
+        <label htmlFor='Blur'>Blur</label>
+        <input
+          type='range'
+          min='5'
+          max='100'
+          value={blur}
+          onChange={(e) => setBlur(e.target.value)}
+        />
+        <label htmlFor='Vertical'></label>
+      </div>
+      <div className='output'>
+        <div className='box'></div>
+      </div>
     </div>
   );
 }
